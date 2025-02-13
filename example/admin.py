d07f9c11000fff79d17dc3f42058235146227dbf
lUkAC1234 <shaxobiddinzuxriddinov@gmail.com>
2025-02-13 21:43:31 +0500

    something

--- example/admin.py
+++ example/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
+from .models import (
+    Contact
+)
 
-# Register your models here.
+@admin.register(Contact)
+class ContactModelAdmin(ModelAdmin):
+    list_display = ['id', 'fullname', 'phone', 'created_at']
+    list_display_links = ['id', 'fullname', 'phone', 'created_at']
+    search_fields = ['id', 'fullname', 'phone']
+    list_filter = ['fullname', 'phone']  
