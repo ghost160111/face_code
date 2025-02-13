c00f33b4895e142806e003e90f1abdead20ece3e
lUkAC1234 <shaxobiddinzuxriddinov@gmail.com>
2025-02-13 20:54:52 +0500

    fuck 2

--- setup.sh
+++ setup.sh
@@ -1,23 +1,32 @@
-#!/bin/bash
+# #!/bin/bash
+# set -e
+
+# echo "Running migrations..."
+# python manage.py makemigrations
+
+# echo "Running migrations..."
+# python manage.py migrate
+
+
+# echo "Collecting static files..."
+# python manage.py collectstatic --noinput
+
+# echo "Creating superuser..."
+# python manage.py shell -c "from django.contrib.auth import get_user_model; \
+# User = get_user_model(); \
+# admin_username = 'admin'; \
+# admin_email = ''; \
+# admin_password = 'facecode'; \
+# if not User.objects.filter(username=admin_username).exists(): \
+#     User.objects.create_superuser(admin_username, admin_email, admin_password); \
+# else: \
+#     print('Superuser already exists')"
+
 set -e
+python3.9 -m ensurepip --upgrade || echo "ensurepip failed"
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput
+python3.9 manage.py makemigrations 
+python3.9 manage.py migrate 
 
-echo "Running migrations..."
-python manage.py makemigrations
-
-echo "Running migrations..."
-python manage.py migrate
-
-
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
-
-echo "Creating superuser..."
-python manage.py shell -c "from django.contrib.auth import get_user_model; \
-User = get_user_model(); \
-admin_username = 'admin'; \
-admin_email = ''; \
-admin_password = 'facecode'; \
-if not User.objects.filter(username=admin_username).exists(): \
-    User.objects.create_superuser(admin_username, admin_email, admin_password); \
-else: \
-    print('Superuser already exists')"
