b10c797e64c7f4748a6dd69dbae02d7351f32152
lUkAC1234 <shaxobiddinzuxriddinov@gmail.com>
2025-02-13 21:01:14 +0500

    changes

--- api/settings.py
+++ api/settings.py
@@ -177,9 +177,10 @@ USE_I18N = True
 USE_TZ = True
 
 # STATIC FILES SETTINGS
-STATIC_URL = 'static/'
-STATIC_DIRS = os.path.join(BASE_DIR, 'static')
-STATICFILES_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Use WhiteNoise’s static file storage in production.
 if not DEBUG:
